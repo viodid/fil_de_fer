86ed1ae8a225a2c2cde30751cd4c744128f0698e
viodid <david.yunta.aller@gmail.com>
2024-03-14 21:23:48 +0100

    testing mlx

--- src/main.c
+++ src/main.c
@@ -6,19 +6,28 @@
 /*   By: dyunta <dyunta@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 19:13:49 by dyunta            #+#    #+#             */
-/*   Updated: 2024/03/14 19:53:10 by dyunta           ###   ########.fr       */
+/*   Updated: 2024/03/14 21:07:35 by dyunta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mlx.h>
 
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bpp;
+	int		line_length;
+	int		endian;
+}	t_data;
+
 int	main(void)
 {
 	void	*mlx;
-	void	*mlx_win;
+	t_data	img;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello World!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.line_length, &img.endian);
 	mlx_loop(mlx);
 
 	return (0);
