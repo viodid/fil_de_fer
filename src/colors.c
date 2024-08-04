68323c28287b7d8248e83093e97349d1a57ebb0d
viodid <david.yunta.aller@gmail.com>
2024-08-04 17:37:00 +0200

    handle colors edge cases

--- src/colors.c
+++ src/colors.c
@@ -6,7 +6,7 @@
 /*   By: dyunta <dyunta@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/04 10:16:12 by dyunta            #+#    #+#             */
-/*   Updated: 2024/08/04 16:43:31 by dyunta           ###   ########.fr       */
+/*   Updated: 2024/08/04 17:36:48 by dyunta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,18 +39,42 @@ static unsigned int	calculate_gradient(t_point a, t_point b, int x)
 	per = percentage(a.x, b.x, x);
 	if (per == 0)
 		return (a.color);
+
 	if ((a.color >> 24) == (b.color >> 24))
 		red = (int)a.color >> 24;
 	else
+	{
 		red = (int)(((a.color >> 24) + (b.color >> 24)) * per);
+		if (red > 0xFF)
+			red = 0xFF;
+		else if (red < (a.color >> 24))
+			red = a.color >> 24;
+	}
+
+
 	if (((a.color & 0x00FFFFFF) >> 16) == ((b.color & 0x00FFFFFF) >> 16))
 		green = (int)((a.color & 0x00FFFFFF) >> 16);
 	else
+	{
 		green = (int)((((a.color & 0x00FFFFFF) >> 16) + ((b.color & 0x00FFFFFF) >> 16)) * per);
+		if (green > 0xFF)
+			green = 0xFF;
+		else if (green < ((a.color & 0xFFFFFF) >> 16))
+			green = green < ((a.color & 0xFFFFFF) >> 16);
+	}
+
+
 	if (((a.color & 0x0000FFFF) >> 8) == ((b.color & 0x0000FFFF) >> 8))
 		blue = (int)((a.color & 0x0000FFFF) >> 8);
 	else
+	{
 		blue = (int)((((a.color & 0x0000FFFF) >> 8) + ((b.color & 0x0000FFFF) >> 8)) * per);
+		if (blue > 0xFF)
+			blue = 0xFF;
+		else if (blue < ((a.color & 0xFFFF) >> 8))
+			green = green < ((a.color & 0xFFFF) >> 8);
+	}
+	unsigned int output = (((red << 24) + (green << 16) + (blue << 8)) + 0xFF);
 	return (((red << 24) + (green << 16) + (blue << 8)) + 0xFF);
 }
 
